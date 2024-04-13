ff35c0176e6be867f3c62ee278fd8114bd9a2e6e
Patrick <abiramiapatrick498@gmail.com>
2024-04-13 19:52:18 +0300

    Styling the front end

--- frontend/footer.php
+++ frontend/footer.php
@@ -1,36 +1,33 @@
-<div class="container-fluid">
+<div class="container-fluid custom-bg-color">
   <footer class="py-5">
     <div class="row">
+
       <div class="col-6 col-md-2 mb-3">
-        <h5>Section</h5>
+        <h4>Section</h4>
         <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0" style="color: white;">Home</a></li>
           <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Contact us!</a></li>
         </ul>
       </div>
 
       <div class="col-6 col-md-2 mb-3">
-        <h5>Section</h5>
+        <h4>Section</h4>
         <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Submit Your Pet for Adoption</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Meet Our Team</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Donate</a></li>
         </ul>
       </div>
 
+
       <div class="col-6 col-md-2 mb-3">
-        <h5>Section</h5>
+        <h4>Explore</h4>
         <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Available Pets</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Adoption Process</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">success stories</a></li>
+
         </ul>
       </div>
 
@@ -48,7 +45,7 @@
     </div>
 
     <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
-      <p>&copy; 2022 Company, Inc. All rights reserved.</p>
+      <p>&copy; 2024 Adopt Don't Shop, Inc. All rights reserved.</p>
       <ul class="list-unstyled d-flex">
         <li class="ms-3"><a class="link-dark" href="#"><i class="bi bi-twitter"></i></a></li>
         <li class="ms-3"><a class="link-dark" href="#"><i class="bi bi-instagram"></i></a></li>
