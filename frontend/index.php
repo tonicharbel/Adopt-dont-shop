aa2c4ebb6e67c108fa980eb58e51f4588c827dd0
tonicharbel <tonicharbel64@gmail.com>
2024-04-22 16:06:53 +0300

    saving

--- frontend/index.php
+++ frontend/index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Adopt don't shop</title>
   <!-- Google font -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
   <!-- Link to Roboto font -->
